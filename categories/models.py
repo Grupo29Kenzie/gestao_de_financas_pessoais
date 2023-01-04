0299b82dd70909b850e615891e486e9af188bf38
emerson gonçalves <emer_gs@hotmail.com>
2023-01-04 15:06:26 -0300

    feat:criação da model e serializer de category

--- categories/models.py
+++ categories/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 
-class Category:
-    ...
+class Category(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    name: models.CharField(max_length=200)
--- categories/serializers.py
+++ categories/serializers.py
@@ -1,2 +1,8 @@
-class CategorySerializer:
-    ...
+from rest_framework import serializers
+from .models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name"]
