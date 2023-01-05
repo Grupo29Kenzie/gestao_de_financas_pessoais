10e36048fa1435d07b8906cfe737620b3f8d87a8
ElyMuniz <elymuzz@hotmail.com>
2023-01-05 14:48:28 -0300

    Atualização de model serializersviews e urls do Credit_card

--- credit_cards/models.py
+++ credit_cards/models.py
@@ -1,5 +1,20 @@
 from django.db import models
+from expenses_entries.models import TransactionOption
 
 
-class Credit_Card:
-    ...
+class Credit_Card(models.Model):
+    due_date = models.PositiveIntegerField()
+    closing_day = models.PositiveBigIntegerField()
+    limit = models.DecimalField(max_digits=12, decimal_places=2)
+    transactions = models.CharField(
+        max_length=15,
+        choices=TransactionOption.choices,
+        default=TransactionOption.EXPENSE,
+    )
+    is_paid = models.BooleanField(default=False)
+    value = models.DecimalField(max_digits=12, decimal_places=2)
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="Credit_cards",
+    )
--- credit_cards/serializers.py
+++ credit_cards/serializers.py
@@ -1,2 +1,21 @@
-class CreditCardSerializer:
-    ...
+from rest_framework import serializers
+from .models import Credit_Card
+
+
+class CreditCardSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        return Credit_Card.objects.create(**validated_data)
+
+    class Meta:
+        model = Credit_Card
+        fields = [
+            "id",
+            "due_date",
+            "closing_day",
+            "limit",
+            "transactions",
+            "is_paid",
+            "value",
+        ]
+
+    read_only_fields = ["id"]
