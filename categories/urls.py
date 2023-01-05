44b0121a366b9e8778511fdd9f741273b988e3e5
emerson gonçalves <emer_gs@hotmail.com>
2023-01-05 10:11:17 -0300

    feat:criacao da model e serializer de categorias

--- categories/urls.py
+++ categories/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("categories/", views.CategoryListView.as_view()),
-    path("users/categories/", views.CategoryCreateView.as_view()),
+    path("categories/", views.CategoryCreateView.as_view()),
 ]
