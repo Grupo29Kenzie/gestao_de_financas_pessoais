3ba16f70249ca9bbfd8917b51e6343ea9d22c37c
Leonardo Carnot <leocarnot8@gmail.com>
2023-01-10 15:32:30 -0300

    feat: Update Routes done

--- credit_cards/serializers.py
+++ credit_cards/serializers.py
@@ -6,6 +6,11 @@ class CreditCardSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return Credit_Card.objects.create(**validated_data)
 
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        return instance
+
     class Meta:
         model = Credit_Card
         fields = [
